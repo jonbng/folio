022fc025619e32250ed9d55a6cff5ce5944fdee2
Jonathan Bangert <jonathan@bangert.dk>
2025-02-26 08:44:24 -0700

    Not do static export

--- next.config.ts
+++ next.config.ts
@@ -2,47 +2,45 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "smooth-storage.aptoma.no",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "media.licdn.com",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "akademia.cc",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "flimmer.app",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "www.home-assistant.io",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "nbskak.dk",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
-    // ],
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "smooth-storage.aptoma.no",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.licdn.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "akademia.cc",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "flimmer.app",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "www.home-assistant.io",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "nbskak.dk",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
-  output: "export",
 };
 
 export default nextConfig;
