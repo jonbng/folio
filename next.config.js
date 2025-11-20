55da42c06a2860002155c31842711bbb0ab69490
Jonathan Bangert <jonathan@bangert.dk>
2025-11-20 10:17:34 +0100

    cache thingy

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Cache Components: Enables explicit opt-in caching with "use cache"
+  // This makes all routes dynamic by default, you opt-into caching
+  cacheComponents: true,
+};
+
+export default nextConfig;
