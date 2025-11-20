89098d357ca7650bb714f1706acd16e15df55550
Jonathan Bangert <jonathan@bangert.dk>
2025-11-21 00:36:43 +0100

    imma go to bed

--- src/app/block/page.tsx
+++ src/app/block/page.tsx
@@ -35,10 +35,51 @@ export default function Block() {
             Hey Block team! I&apos;ve collected a few relevant projects of mine
             that you can look at.
           </p>
-          <p>I</p>
+          <p>
+            Right now I work full time as a lead software engineer at{" "}
+            <a
+              href="https://burstcreators.com/"
+              className="underline font-bold"
+            >
+              Burst
+            </a>
+            , a company that connects brands with creators.
+          </p>
+          <p>
+            Roughly 2 years ago I co-founded{" "}
+            <a href="https://akademia.cc/" className="underline font-bold">
+              Akademia
+            </a>
+            , a platform for schools that unifies how students learn and
+            teachers teach.
+          </p>
+          <p>
+            I&apos;ve been programming since I was 10, and have since then worked on
+            several projects in my free time, including open source projects
+            such as{" "}
+            <a
+              href="https://music-assistant.io/"
+              className="underline font-bold"
+            >
+              Music Assistant
+            </a>
+            .
+          </p>
+          <p>
+            A project I made last year that used AI heavily was{" "}
+            <a
+              href="https://scanshop.arctix.dev/"
+              className="underline font-bold"
+            >
+              ScanShop
+            </a>
+            , a web app that allows you to scan items in your shopping cart and
+            automatically remove them from your shopping list.
+          </p>
           <p className="text-zinc-600 text-sm">
             Please feel free to also check out my main portfolio site by
-            clicking the button above.
+            clicking the button above. Also: I will turn 18 before the start of
+            the fellowship.
           </p>
         </section>
       </div>
