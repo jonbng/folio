90a5dbe163ecce49506268172f665100fc315374
Jonathan Bangert <jonathan@bangert.dk>
2025-11-21 10:40:20 +0100

    yh

--- src/app/block/layout.tsx
+++ src/app/block/layout.tsx
@@ -3,6 +3,19 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Block Builder Fellowship | Jonathan Bangert",
   description: "Block Builder Fellowship | Jonathan Bangert.",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "none",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function BlockLayout({
